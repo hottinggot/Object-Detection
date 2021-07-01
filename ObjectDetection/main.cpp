d26181904d43be403e606f34f1f103771b88ec4c
hottinggot <hottinggot@naver.com>
2021-07-01 17:19:16 +0900

    Initial Commit

---
+++ ObjectDetection/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ObjectDetection
+//
+//  Created by 서정 on 2021/07/01.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
